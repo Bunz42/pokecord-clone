4c408442259aa260a3308efd5faf4272384661a2
Bunz <156132683+Bunz42@users.noreply.github.com>
2026-06-27 15:43:07 -0400

    poke data fetcher script + randomized pokemon spawn logic

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 from discord import Embed, Color, Intents, File
 from discord.ext import commands
 import random
+import json
 
 import os
 from dotenv import load_dotenv
@@ -17,13 +18,9 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# ------------------------------------------------------------------------------------------------------------------------------- #
-
 # -------------------------------------------------FUNCTIONS----------------------------------------------------------- #
 async def function(param):
-    pass
-    
-# --------------------------------------------------------------------------------------------------------------------- #
+    pass 
 
 # -------------------------------------------------EVENTS----------------------------------------------------------- #
 
@@ -37,33 +34,38 @@ async def on_ready():
 async def handle_msg_spawn(message):
     if message.author == bot.user:
         return
-    
 
     if random.random() < 0.5: # tune value for spawn rate (currently 50% every msg)
+        with open('pokemon_data.json' , 'r') as file: # access pokemon data (name and id)
+            data = json.load(file)
+            id = random.randint(1, 1025)
+            name = data[str(id)]
+            
+            print(f"Spawned a: {name.title()}") # print name for testing purposes
 
-        # print("50% chance event happened!")
+            directory = "shiny" if random.random() < 0.5 else "regular" # shiny spawning logic
+            if directory == "shiny":
+                spawn_title = "⭐ A wild SHINY Pokémon appeared! ⭐"
+            else:
+                spawn_title = "A wild Pokémon appeared!"
 
-        directory = "shiny" if random.random() < 0.5 else "regular"
-        file = File(f"assets/scaled_sprites/{directory}/pikachu.gif", filename="pikachu.gif")
 
-        embed = Embed(
-            title="A wild pokemon appeared!", 
-            description="Guess the pokemon and type .catch <pokemon> to catch it!",
-            color=Color.green(),
-        )
+            file = File(f"assets/official-artwork/{directory}/{id}.png", filename=f"{id}.png")
 
-        embed.set_image(url="attachment://pikachu.gif")
+            embed = Embed(
+                title=spawn_title,
+                description="Guess the pokemon and type .catch <pokemon> to catch it!",
+                color=Color.green(),
+            )
 
-        await message.channel.send(file=file, embed=embed)
+            embed.set_image(url=f"attachment://{id}.png")
 
-# ------------------------------------------------------------------------------------------------------------------ #
+            await message.channel.send(file=file, embed=embed)
 
 
 # -------------------------------------------------COMMANDS----------------------------------------------------------- #
 # @bot.command(name="spawn", aliases=["s"])
 
-
-# -------------------------------------------------------------------------------------------------------------------- #
 
 # Run the bot
 if __name__ == '__main__':
